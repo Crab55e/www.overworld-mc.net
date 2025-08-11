d905b6a2ded670d3c3c2d000039f1e0acce54c28
Crab55e <crabkani807@gmail.com>
2025-08-11 18:54:36 +0900

    starlight, mdxやベーシックなセットアップ

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,35 +4,40 @@ import starlight from "@astrojs/starlight";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-      starlight({
-          title: "Overworld MC",
-          social: [
-              {
-                  icon: "discord",
-                  label: "Discord",
-                  href: "https://discord.gg/UEDkWKtKhX",
-              },
-          ],
-          sidebar: [
-              {
-                  label: "Guides",
-                  items: [
-                      // Each item here is one entry in the navigation menu.
-                      { label: "Example Guide", slug: "guides/example" },
-                  ],
-              },
-              {
-                  label: "Reference",
-                  autogenerate: { directory: "reference" },
-              },
-          ],
-      }),
-  ],
+    site: "https://www.overworld-mc.net/",
+    integrations: [starlight({
+        customCss: ["./src/styles/global.css"],
+        title: "Overworld MC",
+        logo: {
+            src: "./src/assets/logo.svg",
+        },
+        social: [
+            {
+                icon: "discord",
+                label: "Discord",
+                href: "https://discord.gg/UEDkWKtKhX",
+            },
+        ],
+        sidebar: [
+            {
+                label: "Guides",
+                items: [
+                    // Each item here is one entry in the navigation menu.
+                    { label: "Example Guide", slug: "guides/example" },
+                ],
+            },
+            {
+                label: "Reference",
+                autogenerate: { directory: "reference" },
+            },
+        ],
+    }), mdx()],
 
-  vite: {
-    plugins: [tailwindcss()],
-  },
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
