74ac6a34b2edc3d653944f3d66078795c3533bed
Crab55e <crabkani807@gmail.com>
2025-08-13 18:42:43 +0900

    やっぱサイドバーを手書きに戻す

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,20 +9,41 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
     site: "https://www.overworld-mc.net/",
-    integrations: [starlight({
-        customCss: ["./src/styles/global.css"],
-        title: "Overworld MC",
-        logo: {
-            src: "./src/assets/logo.svg",
-        },
-        social: [
-            {
-                icon: "discord",
-                label: "Discord",
-                href: "https://discord.gg/UEDkWKtKhX",
+    integrations: [
+        starlight({
+            customCss: ["./src/styles/global.css"],
+            title: "Overworld MC",
+            logo: {
+                src: "./src/assets/logo.svg",
             },
-        ],
-    }), mdx()],
+            social: [
+                {
+                    icon: "discord",
+                    label: "Discord",
+                    href: "https://discord.gg/UEDkWKtKhX",
+                },
+            ],
+            sidebar: [
+                {
+                    label: "トップページ",
+                    link: "/",
+                },
+                {
+                    label: "参加ガイド",
+                    autogenerate: { directory: "join-guides"}
+                },
+                {
+                    label: "Discord",
+                    slug: "discord",
+                },
+                {
+                    label: "コマンドガイド",
+                    slug: "command-guide",
+                },
+            ],
+        }),
+        mdx(),
+    ],
 
     vite: {
         plugins: [tailwindcss()],
