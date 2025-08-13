7fc3687585e33aa190d50fc25d9f8b8f4fa0f4b1
Crab55e <crabkani807@gmail.com>
2025-08-13 14:49:06 +0900

    参加ガイドのファイルを作成

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,19 +22,6 @@ export default defineConfig({
                 href: "https://discord.gg/UEDkWKtKhX",
             },
         ],
-        sidebar: [
-            {
-                label: "Guides",
-                items: [
-                    // Each item here is one entry in the navigation menu.
-                    { label: "Example Guide", slug: "guides/example" },
-                ],
-            },
-            {
-                label: "Reference",
-                autogenerate: { directory: "reference" },
-            },
-        ],
     }), mdx()],
 
     vite: {
