9560e2aa93c0e26dbdb3f13d547791830a292da2
Crab55e <crabkani807@gmail.com>
2025-08-13 18:46:07 +0900

    React の UIフレームワークを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,44 +6,43 @@ import tailwindcss from "@tailwindcss/vite";
 
 import mdx from "@astrojs/mdx";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
     site: "https://www.overworld-mc.net/",
-    integrations: [
-        starlight({
-            customCss: ["./src/styles/global.css"],
-            title: "Overworld MC",
-            logo: {
-                src: "./src/assets/logo.svg",
+    integrations: [starlight({
+        customCss: ["./src/styles/global.css"],
+        title: "Overworld MC",
+        logo: {
+            src: "./src/assets/logo.svg",
+        },
+        social: [
+            {
+                icon: "discord",
+                label: "Discord",
+                href: "https://discord.gg/UEDkWKtKhX",
             },
-            social: [
-                {
-                    icon: "discord",
-                    label: "Discord",
-                    href: "https://discord.gg/UEDkWKtKhX",
-                },
-            ],
-            sidebar: [
-                {
-                    label: "トップページ",
-                    link: "/",
-                },
-                {
-                    label: "参加ガイド",
-                    autogenerate: { directory: "join-guides"}
-                },
-                {
-                    label: "Discord",
-                    slug: "discord",
-                },
-                {
-                    label: "コマンドガイド",
-                    slug: "command-guide",
-                },
-            ],
-        }),
-        mdx(),
-    ],
+        ],
+        sidebar: [
+            {
+                label: "トップページ",
+                link: "/",
+            },
+            {
+                label: "参加ガイド",
+                autogenerate: { directory: "join-guides"}
+            },
+            {
+                label: "Discord",
+                slug: "discord",
+            },
+            {
+                label: "コマンドガイド",
+                slug: "command-guide",
+            },
+        ],
+    }), mdx(), react()],
 
     vite: {
         plugins: [tailwindcss()],
